695a976e4a29977ceac36d4aa6f616d6709dcfbb
giselearaujos <giselearaujo07.s@gmail.com>
2021-05-04 20:34:26 -0300

    🎨 Create CSS the ListUsers

--- components/ListUsers.vue
+++ components/ListUsers.vue
@@ -28,5 +28,87 @@ export default {
 </script>
 
 <style scoped>
+.container {
+  width: 90%;
+  margin: 50px auto;
+}
 
+.wrapper-input {
+  display: flex;
+  justify-content: center;  
+}
+
+.input {
+  width: 30%;
+  margin-bottom: 50px;
+  border-radius: 15px;
+  padding:15px;
+  color: #180F50;
+  font-size: 16px;
+  font-weight: bold;
+}
+
+.row {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  flex-flow: wrap;
+}
+
+.card {
+  width: 20%;
+  background: #fff;
+  border: 1px solid #ccc;
+  border-radius: 15px;
+  margin-bottom: 50px;
+  transition: 0.3s;
+}
+
+.card-header {
+  text-align: center;
+  padding: 50px 10px;
+  background: linear-gradient(to right, #180f50b6,#3113f2b2,#3d9ef2af);
+  border-radius: 15px 15px 0px 0px;
+  color: #fff;
+}
+
+.card-body {
+  padding: 30px 20px;
+  text-align: left;
+  font-size: 14px;
+}
+
+.card-body .btn {
+  display: block;
+  color: #fff;
+  text-align: center;
+  background: linear-gradient(to right,#180f50ad,#3113f2ab,#3d9ef2ab);
+  border-radius: 15px;
+  margin-top: 30px;
+  text-decoration: none;
+  padding: 10px 5px;
+}
+
+.card:hover {
+  transform: scale(1.05);
+  box-shadow: 0 0 40px -10px rgba(0, 0, 0, 0.363);
+}
+
+@media screen and (max-width: 1000px) {
+  .card {
+    width: 40%;
+  }
+}
+
+@media screen and (max-width: 620px) {
+  .container {
+    width: 100%;
+  }
+  .card {
+    width: 80%;
+  }
+  .input {
+    width: 70%;
+  }
+}
 </style>
